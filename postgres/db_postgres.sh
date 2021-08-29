80f62d3d162be78b86e2e36d5fc162e39cf80084
Houston Santos <houston_santos@hotmail.com>
2021-08-29 19:06:37 -0300

    Removendo script postgres

--- postgres/db_postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Criação do database no postgres.
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE elasticsearch;
-EOSQL
